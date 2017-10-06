89eea8db1f2a055250f09c380b250bca45253aec
dmb-220 <dmb2200@gmail.com>
2017-10-06 20:37:20 +0300

    patvarkymai

--- assets/js/mano.js
+++ assets/js/mano.js
@@ -3,11 +3,34 @@ function PrintElem(elem) {
 }
 
 $(document).ready(function() {
+
     $(".add-more").click(function(){
         var html = $(".copy").html();
         $(".after-add-more").after(html);
     });
+
     $("body").on("click",".remove",function(){
         $(this).parents(".control-group").remove();
     });
+
+
+    $('#is_check').change(function() {
+        if($(this).is(":checked")) {$("#inp").show();} else {$("#inp").hide();}
+    });
+
+
+    $(document).ready(function() {
+        $('input[type=radio][name=operacija]').change(function() {
+            if (this.value == '1') {
+                $("#dot").show();
+            }
+            else if (this.value == '2') {
+                $("#dot").show();
+            }else{
+                $("#dot").hide();
+            }
+        });
+    });
+
 });
+
